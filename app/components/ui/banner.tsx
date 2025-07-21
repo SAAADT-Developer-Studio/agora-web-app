eb8fe59fdd5b4a8397626a4ad680cd222f5d9650
marko154 <marko.gartnar777@gmail.com>
2025-07-21 20:38:05 +0200

    fix banner text positioning

--- app/components/ui/banner.tsx
+++ app/components/ui/banner.tsx
@@ -8,13 +8,13 @@ export type BannerProps = {
 export default function Banner(props: Readonly<BannerProps>) {
   return (
     <div className="bg-foreground border-vidikdarkgray/10 flex items-end justify-between rounded-lg border-1 p-4 sm:col-span-full dark:border-0">
-      <div className="flex w-full flex-col">
+      <div className="flex h-full w-full flex-col">
         <div className="flex w-full items-start justify-between">
           <p className="p-lg">{props.title}</p>
         </div>
         <p className="p-sm-regular w-[40%] pt-2">{props.description}</p>
       </div>
-      <div className="flex h-full flex-1 flex-col items-end justify-between gap-3">
+      <div className="flex h-full flex-1 flex-col items-end justify-between gap-4">
         <props.iconComponent className="h-6 w-6" />
         {props.button}
       </div>
