906e60b5981ed84a9cdb3f936dd9710b0372ac35
marko154 <marko.gartnar777@gmail.com>
2025-07-21 19:40:28 +0200

    added high fetch priority on main article image

--- app/components/article-big.tsx
+++ app/components/article-big.tsx
@@ -15,6 +15,7 @@ export default function ArticleBig(props: Readonly<ArticleProps>) {
         backgroundImage: `url(${stock})`,
       }}
     >
+      <link rel="preload" as="image" fetchPriority="high" href={image.src} />
       <div className="text-vidikwhite flex h-full w-full flex-col items-center justify-between rounded-md [background-image:linear-gradient(to_top,rgba(0,0,0,0.6)_0%,rgba(0,0,0,0.5)_15%,rgba(0,0,0,0)_50%)] text-3xl font-bold">
         <div className="flex h-10 w-[98%] items-center justify-between">
           <div className="flex gap-2">
