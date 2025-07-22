82c67be790b7d12510ab0141388a405a6fca53ac
m.gartnar <m.gartnar@sportradar.com>
2025-07-22 15:19:16 +0200

    updated radiuses and improved cta button

--- app/components/ui/CTA.tsx
+++ app/components/ui/CTA.tsx
@@ -4,8 +4,8 @@ import { Link } from "react-router";
 export default function CTA({ buttonText }: Readonly<{ buttonText: string }>) {
   return (
     <Link to="/todo">
-      <div className="text-vidikwhite flex cursor-pointer items-center justify-center gap-2 rounded-lg bg-blue-600 p-2 drop-shadow-lg hover:bg-[#2267fc]">
-        <p className="p-sm uppercase">{buttonText}</p>
+      <div className="text-vidikwhite flex cursor-pointer items-center justify-center gap-2 rounded-lg bg-blue-600 px-3 py-2 drop-shadow-lg hover:bg-[#2267fc]">
+        <p className="text-sm font-semibold uppercase">{buttonText}</p>
         <ArrowRight size={16} />
       </div>
     </Link>
