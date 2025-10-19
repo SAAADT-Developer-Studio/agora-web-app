67555f7c559a0fb63fa66e44f6d8395614f2ac4a
marko154 <marko.gartnar777@gmail.com>
2025-10-19 14:36:50 +0200

    improve theme switch root color

--- app/components/theme-switch.tsx
+++ app/components/theme-switch.tsx
@@ -22,7 +22,7 @@ export function ThemeSwitch({
     <SwitchPrimitive.Root
       data-slot="switch"
       className={cn(
-        "peer focus-visible:border-ring focus-visible:ring-ring/50 dark:bg-vidikdarkgray inline-flex h-7 w-10 shrink-0 items-center rounded-full border border-transparent bg-gray-300 shadow-xs transition-all outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50",
+        "peer focus-visible:border-ring focus-visible:ring-ring/50 dark:bg-vidikdarkgray inline-flex h-7 w-10 shrink-0 items-center rounded-full border border-transparent bg-[oklch(0.8_0.16_257.84_/_1)] shadow-xs transition-all outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50",
         className,
       )}
       onCheckedChange={handleSwitch}
