94963a2f4b7dabfdce4b03f98f12a0c2fa560369
Bine Čadež <bine.cadez@gmail.com>
2025-10-21 18:21:03 +0200

    fixed tags and sources making our website unresponsive for mobile

--- app/components/article-big.tsx
+++ app/components/article-big.tsx
@@ -39,11 +39,12 @@ export default function ArticleBig({
         <link rel="preload" as="image" fetchPriority="high" href={image.src} />
         <div className="text-vidikwhite flex h-full w-full flex-col items-center justify-between rounded-md [background-image:linear-gradient(to_top,rgba(0,0,0,0.7)_0%,rgba(0,0,0,0.55)_15%,rgba(0,0,0,0)_50%)] font-bold">
           <div className="flex w-full items-center justify-between p-2">
-            <div className="flex gap-2">
+            <div className="flex max-h-[23px] flex-wrap gap-2 overflow-hidden">
               {tags.map((tag) => (
-                <Tag key={tag} text={tag} big />
+                <Tag key={tag} text={tag} />
               ))}
             </div>
+
             <Sources
               numberOfArticles={numberOfArticles}
               providerKeys={providerKeys}
@@ -51,7 +52,7 @@ export default function ArticleBig({
           </div>
           <div className="flex w-full flex-col items-start justify-center p-2">
             <div className="flex w-full items-center justify-between">
-              <p className="md:p-lg my-1 line-clamp-2 w-full overflow-clip text-lg leading-8 md:my-4">
+              <p className="my-1 line-clamp-2 w-full overflow-clip text-sm leading-5 md:my-4 md:text-2xl md:leading-8">
                 {title}
               </p>
             </div>
