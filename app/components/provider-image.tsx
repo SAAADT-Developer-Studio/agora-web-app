e18ad6c4af4ed3a78e4e6f1a2bab6cf331974d9c
Bine Čadež <bine.cadez@gmail.com>
2025-10-23 22:05:11 +0200

    added some content to provider page

--- app/components/provider-image.tsx
+++ app/components/provider-image.tsx
@@ -1,16 +1,23 @@
 import { config } from "~/config";
 
-export function getProviderImageUrl(providerKey: string) {
-  return `${config.imagesUrl}/providers/${providerKey}.webp`;
+export function getProviderImageUrl(
+  providerKey: string,
+  size: 60 | 160 = 60,
+): string {
+  return `${config.imagesUrl}/providers/${providerKey}-${size}.webp`;
 }
 
 export function ProviderImage({
   provider,
+  size,
   ...rest
-}: { provider: { key: string; name: string } } & React.ComponentProps<"img">) {
+}: {
+  provider: { key: string; name: string };
+  size?: 60 | 160;
+} & React.ComponentProps<"img">) {
   return (
     <img
-      src={getProviderImageUrl(provider.key)}
+      src={getProviderImageUrl(provider.key, size)}
       alt={provider.name}
       {...rest}
     />
