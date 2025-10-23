4ea0d2487cbaeeefc724283ab69bd45cdb39834a
Bine Čadež <bine.cadez@gmail.com>
2025-10-23 23:08:31 +0200

    random fixes and fixed mobile

--- app/routes/provider.tsx
+++ app/routes/provider.tsx
@@ -175,77 +175,80 @@ export default function ProviderPage({ loaderData }: Route.ComponentProps) {
           provider={provider}
           className="h-[160px] min-w-[160px] rounded-lg"
         />
-        <div className="ml-6 flex flex-col">
-          <div className="flex gap-2">
+        <div className="ml-3 flex flex-col justify-between md:ml-6">
+          <div className="flex flex-wrap gap-2">
             <a
               href={provider.url}
               target="_blank"
-              className="bg-foreground/70 hover:bg-foreground text-primary rounded-lg px-4 py-2 text-lg font-semibold"
+              className="bg-foreground/70 hover:bg-foreground text-primary flex items-center justify-center gap-1 rounded-lg px-2 py-1 text-xs font-semibold md:px-4 md:py-2 md:text-lg"
             >
-              <Globe className="mr-2 mb-1 inline size-5" />
+              <Globe className="size-3 md:size-5" />
               {removeUrlProtocol(provider.url)}
             </a>
             <div
-              className={`${biasKeyToColor(provider.biasRating ?? "")} text-vidikwhite rounded-lg px-4 py-2 text-lg font-semibold`}
+              className={`${biasKeyToColor(provider.biasRating ?? "")} text-vidikwhite flex items-center justify-center rounded-lg px-2 py-1 text-xs font-semibold md:px-4 md:py-2 md:text-lg`}
             >
               {biasKeyToLabel(provider.biasRating ?? "")}
             </div>
           </div>
-          <h1 className="text-[100px] font-bold">{provider.name}</h1>
+          <h1 className="text-[30px] font-bold sm:text-[40px] md:text-[100px]">
+            {provider.name}
+          </h1>
         </div>
       </div>
       <div>
-        <h2 className="text-primary mt-4 text-2xl font-bold">
+        <h2 className="text-primary mt-4 text-lg font-bold md:text-2xl">
           Ali se ne strinjaš da {provider.name} spada pod{" "}
           {biasKeyToLabel(provider.biasRating ?? "")}?
         </h2>
-        <p className="text-primary/50 text-lg">
+        <p className="text-primary/50 text-sm md:text-lg">
           Glasuj kam spada na političnem spektru
         </p>
       </div>
       <div className="mt-4 grid w-full grid-cols-5 gap-2">
         <div
           onClick={() => setVoted(true)}
-          className="text-vidikwhite transition-duration-200 flex h-48 cursor-pointer items-center justify-center rounded-lg bg-[#FA2D36] text-xl font-semibold transition-transform hover:scale-102"
+          className="text-vidikwhite transition-duration-200 flex aspect-square cursor-pointer items-center justify-center rounded-lg bg-[#FA2D36] text-center text-sm leading-4 font-semibold transition-transform hover:scale-102 md:text-xl md:leading-5"
         >
           leva
         </div>
         <div
           onClick={() => setVoted(true)}
-          className="text-vidikwhite transition-duration-200 flex cursor-pointer items-center justify-center rounded-lg bg-[#FF6166] text-xl font-semibold transition-transform hover:scale-102"
+          className="text-vidikwhite transition-duration-200 flex aspect-square cursor-pointer items-center justify-center rounded-lg bg-[#FF6166] text-center text-sm leading-4 font-semibold transition-transform hover:scale-102 md:text-xl md:leading-5"
         >
           center leva
         </div>
         <div
           onClick={() => setVoted(true)}
-          className="transition-duration-200 flex cursor-pointer items-center justify-center rounded-lg bg-[#FEFFFF] text-xl font-semibold !text-black transition-transform hover:scale-102"
+          className="transition-duration-200 flex aspect-square cursor-pointer items-center justify-center rounded-lg bg-[#FEFFFF] text-center text-sm leading-4 font-semibold !text-black transition-transform hover:scale-102 md:text-xl md:leading-5"
         >
           center
         </div>
         <div
           onClick={() => setVoted(true)}
-          className="text-vidikwhite transition-duration-200 flex cursor-pointer items-center justify-center rounded-lg bg-[#52A1FF] text-xl font-semibold transition-transform hover:scale-102"
+          className="text-vidikwhite transition-duration-200 flex aspect-square cursor-pointer items-center justify-center rounded-lg bg-[#52A1FF] text-center text-sm leading-4 font-semibold transition-transform hover:scale-102 md:text-xl md:leading-5"
         >
           center desna
         </div>
         <div
           onClick={() => setVoted(true)}
-          className="text-vidikwhite transition-duration-200 flex cursor-pointer items-center justify-center rounded-lg bg-[#2D7EFF] text-xl font-semibold transition-transform hover:scale-102"
+          className="text-vidikwhite transition-duration-200 flex aspect-square cursor-pointer items-center justify-center rounded-lg bg-[#2D7EFF] text-center text-sm leading-4 font-semibold transition-transform hover:scale-102 md:text-xl md:leading-5"
         >
           desna
         </div>
       </div>
       {voted && (
         <div className="animate-in slide-in-from-top-4 fade-in mt-12 duration-500">
-          <h2 className="text-primary text-2xl font-bold">
+          <h2 className="text-primary text-lg font-bold md:text-2xl">
             Glasuj še za druge medije!
           </h2>
-          <div className="mt-6 grid w-full grid-cols-5 gap-2">
+          <div className="mt-6 grid w-full grid-cols-2 items-center gap-4 sm:grid-cols-3 md:grid-cols-5">
             {otherProviders.map((p) => (
               <Link
                 key={p.key}
                 to={`/medij/${p.key}`}
                 onClick={() => setVoted(false)}
+                className="flex items-center justify-center"
               >
                 <div
                   className="relative h-[160px] w-[160px] cursor-pointer rounded-lg bg-cover bg-center transition-transform duration-200 hover:scale-102"
@@ -268,7 +271,7 @@ export default function ProviderPage({ loaderData }: Route.ComponentProps) {
         </div>
       )}
       <div className="mt-12 grid grid-cols-1 gap-6 md:grid-cols-3">
-        <Card className="bg-foreground border-none">
+        <Card className="bg-foreground border-none !py-0">
           <div className="space-y-4 p-8">
             <div className="flex justify-end">
               <Newspaper className="h-6 w-6 text-white" />
@@ -288,7 +291,7 @@ export default function ProviderPage({ loaderData }: Route.ComponentProps) {
           </div>
         </Card>
 
-        <Card className="bg-foreground border-none">
+        <Card className="bg-foreground border-none !py-0">
           <div className="space-y-4 p-8">
             <div className="flex justify-end">
               <Newspaper className="h-6 w-6 text-white" />
@@ -303,12 +306,12 @@ export default function ProviderPage({ loaderData }: Route.ComponentProps) {
               <p className="text-lg leading-tight font-medium text-white">
                 Objavljenih člankov ta teden
               </p>
-              <p className="text-sm text-gray-400">Rank: #{stats.week.count}</p>
+              <p className="text-sm text-gray-400">Rank: #{stats.week.rank}</p>
             </div>
           </div>
         </Card>
 
-        <Card className="bg-foreground border-none">
+        <Card className="bg-foreground border-none !py-0">
           <div className="space-y-4 p-8">
             <div className="flex justify-end">
               <Newspaper className="h-6 w-6 text-white" />
