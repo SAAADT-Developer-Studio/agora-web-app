8273b604f72681c3e68d66b9acf4224af95131a3
marko154 <marko.gartnar777@gmail.com>
2025-10-24 14:09:37 +0200

    added link to provider stats chart, made tooltip trigger the whole chart bar

--- app/components/provider-stats-card.tsx
+++ app/components/provider-stats-card.tsx
@@ -1,6 +1,6 @@
 import { ChartBar } from "lucide-react";
 import { Suspense } from "react";
-import { Await } from "react-router";
+import { Await, href, Link } from "react-router";
 import { ProviderImage } from "~/components/provider-image";
 import { ErrorUI } from "~/components/ui/error-ui";
 import { SideCardContainer, SideCardHeader } from "~/components/ui/side-card";
@@ -10,7 +10,6 @@ import {
   TooltipContent,
   TooltipTrigger,
 } from "~/components/ui/tooltip";
-import { config } from "~/config";
 import type { getProviderStats } from "~/lib/services/homePageProviderStats";
 
 export function ProviderStatsCard({
@@ -67,26 +66,34 @@ function ProviderStatsChart({
     <div className="flex flex-col gap-3 px-5 pb-4 md:pb-0">
       {providerStats.map((provider) => {
         return (
-          <div className="group flex gap-2" key={provider.key}>
+          <div key={provider.key}>
             <Tooltip>
-              <TooltipTrigger>
-                <ProviderImage
-                  provider={provider}
-                  className="size-8 overflow-clip rounded border-2 border-black/20 transition-all group-hover:border-black/40 dark:border-white/70 dark:group-hover:border-white"
-                />
+              <TooltipTrigger asChild>
+                <Link
+                  to={href("/medij/:providerKey", {
+                    providerKey: provider.key,
+                  })}
+                  className="group flex gap-2"
+                >
+                  <ProviderImage
+                    provider={provider}
+                    className="size-8 overflow-clip rounded border-2 border-black/20 transition-all group-hover:border-black/40 dark:border-white/70 dark:group-hover:border-white"
+                  />
+
+                  <div className="flex flex-1 rounded-md bg-black/5 dark:bg-[#5a5a5a]">
+                    <div
+                      className="bg-electricblue flex h-8 items-center justify-end rounded-md px-2 font-semibold text-white transition-all group-hover:shadow-[0_0_8px_var(--color-electricblue)]"
+                      style={{
+                        width: `${(provider.articleCount / maxArticles) * 100}%`,
+                      }}
+                    >
+                      {provider.articleCount}
+                    </div>
+                  </div>
+                </Link>
               </TooltipTrigger>
               <TooltipContent side="left">{provider.name}</TooltipContent>
             </Tooltip>
-            <div className="flex flex-1 rounded-md bg-black/5 dark:bg-[#5a5a5a]">
-              <div
-                className="bg-electricblue flex h-8 items-center justify-end rounded-md px-2 font-semibold text-white transition-all group-hover:shadow-[0_0_8px_var(--color-electricblue)]"
-                style={{
-                  width: `${(provider.articleCount / maxArticles) * 100}%`,
-                }}
-              >
-                {provider.articleCount}
-              </div>
-            </div>
           </div>
         );
       })}
